7eb15593cbd048cd8dbb77a3d8450b66b31c9ffe
--global <dsaiaditya@gmail.com>
2024-03-12 15:16:20 +0530

    reset password bug fix

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -229,7 +229,8 @@ module.exports.resetPassword = async (req, res) => {
                 }
                 break;
             case 'password':
-                await Users.findOneAndUpdate({ _id }, { password, cpassword: password });
+                const hashedPassword = await bcrypt.hash(password, 12);
+                await Users.findOneAndUpdate({ _id }, { password: hashedPassword, cpassword: hashedPassword }, { new: true });
                 message = 'password changed'
                 success = true;
                 break;

c6d98363fbeddd5bd3571efa4620e791754cf0a5
--global <dsaiaditya@gmail.com>
2024-03-13 15:05:33 +0530

    removed unwanted packages

--- client/src/index.js
+++ client/src/index.js
@@ -21,11 +21,6 @@ import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.m
 import 'swiper/css/bundle';
 
 
-import 'react-datetime-picker/dist/DateTimePicker.css';
-import 'react-calendar/dist/Calendar.css';
-import 'react-clock/dist/Clock.css';
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>

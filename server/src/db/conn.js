4fbf94f787198c98ff89b8266f5fee0456f2a66b
--global <dsaiaditya@gmail.com>
2024-03-13 18:06:01 +0530

    databse URI set from env

--- server/src/db/conn.js
+++ server/src/db/conn.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const databaseUrl = 'mongodb://127.0.0.1:27017/'
+const DB = process.env.LOCAL_DB || process.env.DATABASE_URI;
 
-mongoose.connect(`${databaseUrl}${process.env.DATABASE_NAME}`, {
+mongoose.connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
